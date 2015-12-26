0c8cc57d2ddae96b7ec1706624797952488238ff
Andy Zeng <andy.zeng.workhorse@gmail.com>
2015-12-26 02:11:36 -0500

    switch back to N^2 fragment comparisons

--- matcher.cpp
+++ matcher.cpp
@@ -27,26 +27,27 @@ int main()
         allFragments.push_back(fragmentFilename);
     }
     
-    // for (int i = 0; i < fragmentCount; i++)
-    // {
-    //     for (int j = 0; j < fragmentCount; j++)
-    //     {
-    //         const string resultFilename = "results/match" + to_string(i) + "-" + to_string(j) + ".txt";
-    //         if (j <= i || util::fileExists(resultFilename))
-    //             continue;
-            
-    //         auto result = FragmentMatcher::match(allFragments[i], allFragments[j], i, j, voxelSize, truncationRadius, maxKeypointMatchDist);
-    //         result.saveASCII(resultFilename);
-    //     }
-    // }
-
-    for (int i = 0; i < fragmentCount - 1; i++)
+    for (int i = 0; i < fragmentCount; i++)
     {
-        bool dir_success = system("mkdir results");
-        const string resultFilename = "results/match" + to_string(i) + "-" + to_string(i+1) + ".txt";
-        auto result = FragmentMatcher::match(allFragments[i], allFragments[i+1], i, i+1, voxelSize, truncationRadius, maxKeypointMatchDist);
-        result.saveASCII(resultFilename);
+        for (int j = 0; j < fragmentCount; j++)
+        {
+            bool dir_success = system("mkdir results");
+            const string resultFilename = "results/match" + to_string(i) + "-" + to_string(j) + ".txt";
+            if (j <= i || util::fileExists(resultFilename))
+                continue;
+            
+            auto result = FragmentMatcher::match(allFragments[i], allFragments[j], i, j, voxelSize, truncationRadius, maxKeypointMatchDist);
+            result.saveASCII(resultFilename);
+        }
     }
+
+    // for (int i = 0; i < fragmentCount - 1; i++)
+    // {
+    //     bool dir_success = system("mkdir results");
+    //     const string resultFilename = "results/match" + to_string(i) + "-" + to_string(i+1) + ".txt";
+    //     auto result = FragmentMatcher::match(allFragments[i], allFragments[i+1], i, i+1, voxelSize, truncationRadius, maxKeypointMatchDist);
+    //     result.saveASCII(resultFilename);
+    // }
 
     return 0;
 }
